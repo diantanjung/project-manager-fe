b102fa70d4b51123c3e2a9dc58b8851d4367c347
Dian Tanjung <dianmulyanat@gmail.com>
2026-01-28 18:01:08 +0700

    add ignore and env file

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -10,7 +10,7 @@ export const setAccessToken = (token: string | null) => {
 export const getAccessToken = () => accessToken;
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
